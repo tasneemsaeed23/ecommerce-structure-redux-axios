8ec882d17377baaca7570b922bce40ea4f6c4289
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-13 17:00:11 +0300

    "Update .gitignore to include .env and .env.development, and remove .local from environment file patterns"

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,12 +1,13 @@
-import { GET_ALL_CATEGORY, GET_ERROR } from "../type";
+import { GET_ALL_CATEGORY, GET_ERROR, CREATE_CATEGORY } from "../type";
 import useGetData from "../../hooks/useGetData";
-const getAllCategory = () => async (dispatch) => {
+import { useInsertDataWithImage } from "../../hooks/useInsertData";
+//get all category
+export const getAllCategory = (limit) => async (dispatch) => {
   try {
-    // const res = await baseURL.get("/api/v1/categories");
-    const response = await useGetData("/api/v1/categories");
+    const response = await useGetData(`/api/v1/categories?limit=${limit}`);
     dispatch({
       type: GET_ALL_CATEGORY,
-      payload: response.data,
+      payload: response,
     });
   } catch (e) {
     dispatch({
@@ -16,4 +17,40 @@ const getAllCategory = () => async (dispatch) => {
   }
 };
 
-export default getAllCategory;
+//get all category with pagination
+export const getAllCategoryPage = (page) => async (dispatch) => {
+  try {
+    const response = await useGetData(
+      `/api/v1/categories?limit=3&page=${page}`
+    );
+    dispatch({
+      type: GET_ALL_CATEGORY,
+      payload: response,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: `Error: ${e.message}`,
+    });
+  }
+};
+
+//get all category with pagination
+export const creatCategory = (formData) => async (dispatch) => {
+  try {
+    const response = await useInsertDataWithImage(
+      `/api/v1/categories`,
+      formData
+    );
+    dispatch({
+      type: CREATE_CATEGORY,
+      payload: response,
+      loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: `Error: ${e.message}`,
+    });
+  }
+};
