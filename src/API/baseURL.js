77e9ef23ba3fdc61012134cd6d0e7ca16fe3a3bc
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-13 16:58:25 +0300

    "Updated .gitignore, added functionality to AdminAddCategory component, refactored CategoryContainer and HomeCategory components, updated Pagination component, and added new actions to categoryAction.js"

--- src/API/baseURL.js
+++ src/API/baseURL.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const baseUrl = axios.create({ baseURL: "http://localhost:3001/" });
+const baseUrl = axios.create({
+  baseURL: "http://localhost:8000",
+});
 
 export default baseUrl;
