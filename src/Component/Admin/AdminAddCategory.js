036398718eb0ee9c4a292637a3dce0cb66516e57
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-13 16:58:44 +0300

    "Updated .gitignore, refactored CategoryContainer and HomeCategory components, added pagination functionality, and modified categoryAction.js to include getAllCategoryPage and creatCategory actions."

--- src/Component/Admin/AdminAddCategory.js
+++ src/Component/Admin/AdminAddCategory.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import avatar from "../../images/avatar.png";
+import { creatCategory } from "../../redux/actions/categoryAction";
+import { useSelector, useDispatch } from "react-redux";
+
 const AdminAddCategory = () => {
+  const dispatch = useDispatch();
+  const [img, setImg] = useState(avatar);
+  const [Name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState("");
+
+  // Update image with selected file
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImg(URL.createObjectURL(event.target.files[0]));
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  //save data in database
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", Name);
+    formData.append("image", selectedFile);
+    dispatch(creatCategory(formData));
+  };
   return (
     <div>
       <Row className="justify-content-start ">
         <div className="admin-content-text pb-4">اضافه تصنيف جديد</div>
         <Col sm="8">
           <div className="text-form pb-2">صوره التصنيف</div>
-          <img src={avatar} alt="" height="100px" width="120px" />
+          <div>
+            <label htmlFor="upload-photo">
+              <img
+                src={img}
+                alt="category"
+                height="100px"
+                width="120px"
+                style={{ cursor: "pointer" }}
+              />
+            </label>
+            <input
+              type="file"
+              name="photo"
+              onChange={onImageChange}
+              id="upload-photo"
+            />
+          </div>
           <input
+            onChange={(e) => setName(e.target.value)}
+            value={Name}
             type="text"
             className="input-form d-block mt-3 px-3"
             placeholder="اسم التصنيف"
@@ -18,7 +60,9 @@ const AdminAddCategory = () => {
       </Row>
       <Row>
         <Col sm="8" className="d-flex justify-content-end ">
-          <button className="btn-save d-inline mt-2 ">حفظ التعديلات</button>
+          <button onClick={handelSubmit} className="btn-save d-inline mt-2 ">
+            حفظ التعديلات
+          </button>
         </Col>
       </Row>
     </div>
