50089d7121b7adb307d37b531e71c013f804827a
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-15 21:21:57 +0300

    Edit

--- src/Component/Brand/BrandFeatured.js
+++ src/Component/Brand/BrandFeatured.js
@@ -8,24 +8,20 @@ const BrandFeatured = ({ title, btntitle }) => {
   const [loading, brand] = HomeBrandHook();
   return (
     <Container>
-      {brand.data.length > 0 ? (
-        <div>
-          <SubTiltle title={title} btntitle={btntitle} pathText="/allbrand" />
-          <Row className="my-1 d-flex justify-content-between">
-            {loading === false ? (
-              brand.data ? (
-                brand.data.slice(0, 4).map((item, index) => {
-                  return <BrandCard key={index} img={item.image} />;
-                })
-              ) : (
-                <h4>لا يوجد ماركات</h4>
-              )
-            ) : (
-              <Spinner animation="border" variant="warning" />
-            )}
-          </Row>
-        </div>
-      ) : null}
+      <SubTiltle title={title} btntitle={btntitle} pathText="/allbrand" />
+      <Row className="my-1 d-flex justify-content-between">
+        {loading === false ? (
+          brand.data ? (
+            brand.data.slice(0, 4).map((item, index) => {
+              return <BrandCard key={index} img={item.image} />;
+            })
+          ) : (
+            <h4>لا يوجد ماركات</h4>
+          )
+        ) : (
+          <Spinner animation="border" variant="warning" />
+        )}
+      </Row>
     </Container>
   );
 };
