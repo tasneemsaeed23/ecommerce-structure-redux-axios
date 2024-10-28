fcb01b20508678a7c2c1c5801be404d998effaf0
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-28 23:21:02 +0300

    Add dynamic search result count to ShopProductsPage

--- src/page/Products/ShopProductsPage.js
+++ src/page/Products/ShopProductsPage.js
@@ -5,13 +5,15 @@ import { Container, Row, Col } from "react-bootstrap";
 import Pagination from "../../Component/Uitlity/Pagination";
 import SideFilter from "../../Component/Uitlity/SideFilter";
 import CardProductContainer from "./../../Component/Products/CardProductContainer";
+import ViewSearchProductsHook from "../../hook/products/view-search-products-hook";
 
 const ShopProductsPage = () => {
+  const [items] = ViewSearchProductsHook();
   return (
     <div style={{ minHeight: "670px" }}>
       <CategoryHeader />
       <Container>
-        <SearchCountResult title="400 نتجية بحث" />
+        <SearchCountResult title={`هناك ${items.length} نتيجة بحث `} />
         <Row className="d-flex flex-row">
           <Col sm="2" xs="2" md="1" className="d-flex">
             <SideFilter />
