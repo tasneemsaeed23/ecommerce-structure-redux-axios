5ebba19c9c2d524c5784b706bd7f55ea8ab336fa
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-28 18:31:40 +0300

    "Added GET_SUB_CATEGORY action and reducer, and updated imports and type definitions accordingly."

--- src/Component/Admin/AdminAddProducts.js
+++ src/Component/Admin/AdminAddProducts.js
@@ -1,31 +1,40 @@
-import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Multiselect from "multiselect-react-dropdown";
-import avatar from "../../images/avatar.png";
 import add from "../../images/add.png";
 import MultiImageInput from "react-multiple-image-input";
 
+import { CompactPicker } from "react-color";
+import { ToastContainer } from "react-toastify";
+import AdminAddProductsHook from "./../../hook/products/add-products-hook";
+
 const AdminAddProducts = () => {
-  const onSelect = () => {};
-  const onRemove = () => {};
-
-  const options = [
-    { name: "التصنيف الاول", id: 1 },
-    { name: "التصنيف الثاني", id: 2 },
-  ];
-  //values images products
-  const [images, setImages] = useState([]);
-
-  //values state
-  const [prodName, setprodName] = useState("");
-  const [prodDescription, setprodDescription] = useState("");
-  const [priceBefore, setpriceBefore] = useState("السعر قبل الخصم");
-  const [priceAftr, setpriceAftr] = useState("السعر بعد الخصم");
-  const [qty, setQty] = useState("الكمية المتاحة");
-  const [catID, setCatID] = useState("");
-  const [BrandID, setBrandID] = useState("");
-  const [subCatID, setSubCatID] = useState([]);
-  const [selectedSubID, setselectedSubID] = useState([]);
+  const [
+    onChangeDesName,
+    onChangeQty,
+    onChangeColor,
+    onChangePriceAfter,
+    onChangePriceBefor,
+    onChangeProdName,
+    showColor,
+    category,
+    brand,
+    priceAftr,
+    images,
+    setImages,
+    onSelect,
+    onRemove,
+    options,
+    handelChangeComplete,
+    removeColor,
+    onSeletCategory,
+    handelSubmit,
+    onSeletBrand,
+    colors,
+    priceBefore,
+    qty,
+    prodDescription,
+    prodName,
+  ] = AdminAddProductsHook();
 
   return (
     <div>
@@ -34,57 +43,65 @@ const AdminAddProducts = () => {
         <Col sm="8">
           <div className="text-form pb-2"> صور للمنتج</div>
 
-          <MultiImageInput images={images} setImages={setImages} />
-
-          <input
-            value={prodName}
-            onChange={(e) => setprodName(e.target.value)}
-            type="text"
-            className="input-form d-block mt-3 px-3"
-            placeholder="اسم المنتج"
-            them={"light"}
+          <MultiImageInput
+            images={images}
+            setImages={setImages}
+            theme={"light"}
             allowCrop={false}
             max={4}
           />
+
+          <input
+            value={prodName}
+            onChange={onChangeProdName}
+            type="text"
+            className="input-form d-block mt-3 px-3"
+            placeholder="اسم المنتج"
+          />
           <textarea
-            value={prodDescription}
-            onChange={(e) => setprodDescription(e.target.value)}
             className="input-form-area p-2 mt-3"
             rows="4"
             cols="50"
             placeholder="وصف المنتج"
+            value={prodDescription}
+            onChange={onChangeDesName}
           />
           <input
             type="number"
             className="input-form d-block mt-3 px-3"
             placeholder="السعر قبل الخصم"
             value={priceBefore}
-            onChange={(e) => setpriceBefore(e.target.value)}
+            onChange={onChangePriceBefor}
           />
           <input
             type="number"
             className="input-form d-block mt-3 px-3"
-            placeholder="سعر بعد الخصم"
+            placeholder="السعر بعد الخصم"
             value={priceAftr}
-            onChange={(e) => setpriceAftr(e.target.value)}
+            onChange={onChangePriceAfter}
           />
           <input
             type="number"
             className="input-form d-block mt-3 px-3"
-            placeholder=" الكمية المتاحة"
+            placeholder="الكمية المتاحة"
             value={qty}
-            onChange={(e) => setQty(e.target.value)}
+            onChange={onChangeQty}
           />
           <select
-            name="languages"
-            id="lang"
+            name="cat"
+            onChange={onSeletCategory}
             className="select input-form-area mt-3 px-2 "
           >
-            <option value="val">التصنيف الرئيسي</option>
-            <option value="val">التصنيف الاول</option>
-            <option value="val2">التصنيف الثاني</option>
-            <option value="val2">التصنيف الثالث</option>
-            <option value="val2">التصنيف الرابع</option>
+            <option value="0">التصنيف الرئيسي</option>
+            {category.data
+              ? category.data.map((item, index) => {
+                  return (
+                    <option key={index} value={item._id}>
+                      {item.name}
+                    </option>
+                  );
+                })
+              : null}
           </select>
 
           <Multiselect
@@ -98,37 +115,57 @@ const AdminAddProducts = () => {
           />
           <select
             name="brand"
-            id="brand"
+            onChange={onSeletBrand}
             className="select input-form-area mt-3 px-2 "
           >
-            <option value="val">الماركة</option>
-            <option value="val2">التصنيف الماركة الاولي</option>
-            <option value="val2">التصنيف الماركة الثانيه</option>
-            <option value="val2">التصنيف الرابع</option>
+            <option value="0">اختر ماركة</option>
+            {brand.data
+              ? brand.data.map((item, index) => {
+                  return (
+                    <option key={index} value={item._id}>
+                      {item.name}
+                    </option>
+                  );
+                })
+              : null}
           </select>
           <div className="text-form mt-3 "> الالوان المتاحه للمنتج</div>
           <div className="mt-1 d-flex">
-            <div
-              className="color ms-2 border  mt-1"
-              style={{ backgroundColor: "#E52C2C" }}
-            ></div>
-            <div
-              className="color ms-2 border mt-1 "
-              style={{ backgroundColor: "white" }}
-            ></div>
-            <div
-              className="color ms-2 border  mt-1"
-              style={{ backgroundColor: "black" }}
-            ></div>
-            <img src={add} alt="" width="30px" height="35px" className="" />
+            {colors.length >= 1
+              ? colors.map((color, index) => {
+                  return (
+                    <div
+                      key={index}
+                      onClick={() => removeColor(color)}
+                      className="color ms-2 border  mt-1"
+                      style={{ backgroundColor: color }}
+                    ></div>
+                  );
+                })
+              : null}
+
+            <img
+              onClick={onChangeColor}
+              src={add}
+              alt=""
+              width="30px"
+              height="35px"
+              style={{ cursor: "pointer" }}
+            />
+            {showColor === true ? (
+              <CompactPicker onChangeComplete={handelChangeComplete} />
+            ) : null}
           </div>
         </Col>
       </Row>
       <Row>
         <Col sm="8" className="d-flex justify-content-end ">
-          <button className="btn-save d-inline mt-2 ">حفظ التعديلات</button>
+          <button onClick={handelSubmit} className="btn-save d-inline mt-2 ">
+            حفظ التعديلات
+          </button>
         </Col>
       </Row>
+      <ToastContainer />
     </div>
   );
 };
