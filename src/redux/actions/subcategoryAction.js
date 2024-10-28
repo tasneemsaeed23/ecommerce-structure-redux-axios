85fee860c3703bc13f3c17e788911a60c5caa982
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-28 18:36:59 +0300

    add getOneCategory action for fetching subcategories by category ID

--- src/redux/actions/subcategoryAction.js
+++ src/redux/actions/subcategoryAction.js
@@ -1,11 +1,30 @@
-import { CREATE_SUB_CATEGORY, GET_ERROR } from "../type";
+import { CREATE_SUB_CATEGORY, GET_ERROR, GET_SUB_CATEGORY } from "../type";
 import { useInsertData } from "../../hooks/useInsertData";
+import useGetData from "../../hooks/useGetData";
 //create Subcategory with pagination
 export const creatSubCategory = (data) => async (dispatch) => {
   try {
     const response = await useInsertData(`/api/v1/subcategories`, data);
     dispatch({
       type: CREATE_SUB_CATEGORY,
+      payload: response,
+      loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: `Error: ${e.message}`,
+    });
+  }
+};
+
+//get sub category depend in vat id
+export const getOneCategory = (id) => async (dispatch) => {
+  try {
+    const response = await useGetData(`/api/v1/categories/${id}/subcategories`);
+    console.log(response.data);
+    dispatch({
+      type: GET_SUB_CATEGORY,
       payload: response,
       loading: true,
     });
