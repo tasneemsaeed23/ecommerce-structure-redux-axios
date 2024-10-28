85e9d9bd55cd366e656e3394e6cf56e3e9a59144
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-28 18:15:22 +0300

    "Added react-color dependency, updated AdminAddProducts component with color picker, and modified subcategory action and reducer"

---
+++ src/redux/reducers/productsReducer.js
@@ -0,0 +1,34 @@
+import { CREATE_PRODUCTS, GET_ERROR, GET_ALL_PRODUCTS } from "../type";
+
+const inital = {
+  products: [],
+  allProducts: [],
+  loading: true,
+};
+
+const productsReducer = (state = inital, action) => {
+  switch (action.type) {
+    case CREATE_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+        loading: false,
+      };
+    case GET_ALL_PRODUCTS:
+      return {
+        ...state,
+        allProducts: action.payload,
+        loading: false,
+      };
+
+    case GET_ERROR:
+      return {
+        loading: false,
+        products: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
