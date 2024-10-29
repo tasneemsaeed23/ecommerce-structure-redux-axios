f29834df5f293e4a34bc92d81423e1c614e1fe0a
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-29 22:59:23 +0300

    Enhanced ViewProductsDetalisHook to dynamically fetch and display product and category data.

--- src/hook/products/view-products-detalis-hook.js
+++ src/hook/products/view-products-detalis-hook.js
@@ -2,18 +2,25 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOneProduct } from "../../redux/actions/productsAction";
 import mobile from "../../images/mobile.png";
+import { getOneCategory } from "../../redux/actions/categoryAction";
 
-const ViewProductsDetalisHook = (id) => {
+const ViewProductsDetalisHook = (prodID) => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getOneProduct(id));
+    dispatch(getOneProduct(prodID));
   }, []);
 
   const oneProducts = useSelector((state) => state.allproducts.oneProduct);
+  const oneCategory = useSelector((state) => state.allCategory.oneCategory);
 
+  //to show products item
   let item = [];
   if (oneProducts.data) item = oneProducts.data;
   else item = [];
+
+  useEffect(() => {
+    if (item.category) dispatch(getOneCategory(item.category));
+  }, [item]);
 
   //to view images gallery
   let images = [];
@@ -31,7 +38,12 @@ const ViewProductsDetalisHook = (id) => {
     ];
   }
 
-  return [item, images];
+  //to show category item
+  let cat = [];
+  if (oneCategory.data) cat = oneCategory.data;
+  else cat = [];
+
+  return [item, cat, images];
 };
 
 export default ViewProductsDetalisHook;
