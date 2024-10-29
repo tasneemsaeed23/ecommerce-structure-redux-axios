ad19e810e1280fa97a7d773127c8e40816c3270b
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-29 17:41:02 +0300

    to load images dynamically

--- src/Component/Products/ProductGallery.js
+++ src/Component/Products/ProductGallery.js
@@ -1,24 +1,15 @@
 import React from "react";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
-import mobile from "../../images/mobile.png";
-import mobile1 from "../../images/mobile1.png";
-import mobile2 from "../../images/mobile2.png";
 import LeftButton from "./LeftButton";
 import RightButton from "./RightButton";
+import { useParams } from "react-router-dom";
+import ViewProductsDetalisHook from "../../hook/products/view-products-detalis-hook";
 
-const ProductGallery = ({ id }) => {
-  const images = [
-    {
-      original: `${mobile}`,
-    },
-    {
-      original: `${mobile1}`,
-    },
-    {
-      original: `${mobile2}`,
-    },
-  ];
+const ProductGallery = () => {
+  const { id } = useParams();
+  const [item, images] = ViewProductsDetalisHook(id);
+
   return (
     <div
       className="product-gallary-card d-flex justfiy-content-center  align-items-center
@@ -27,7 +18,6 @@ const ProductGallery = ({ id }) => {
       {" "}
       <ImageGallery
         items={images}
-        defaultImage={mobile}
         showFullscreenButton={false}
         isRTL={true}
         showPlayButton={false}
