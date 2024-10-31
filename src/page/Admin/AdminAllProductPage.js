ef9e52cb913f62bd884cbb7bc6e94816051a5524
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-31 23:23:25 +0300

    "Added DELETE_PRODUCTS action and reducer, updated productsAction.js and productsReducer.js files"

--- src/page/Admin/AdminAllProductPage.js
+++ src/page/Admin/AdminAllProductPage.js
@@ -3,7 +3,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import AdminSideBar from "../../Component/Admin/AdminSideBar";
 import AdminAllProducts from "../../Component/Admin/AdminAllProducts";
 import Pagination from "../../Component/Uitlity/Pagination";
+import viewProductAdminHook from "../../hook/admin/view-product-admin-hook";
 const AdminAllProductsPage = () => {
+  const [items, pagination] = viewProductAdminHook();
   return (
     <Container>
       <Row className="py-3">
@@ -12,7 +14,7 @@ const AdminAllProductsPage = () => {
         </Col>
 
         <Col sm="9" xs="10" md="10">
-          <AdminAllProducts />
+          <AdminAllProducts products={items} />
           <Pagination />
         </Col>
       </Row>
