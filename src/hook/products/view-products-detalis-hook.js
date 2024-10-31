092fa96cf8141ae7e21f5197b0aae72f0fe1ad4b
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-31 15:18:39 +0300

    Extend `ViewProductsDetalisHook`: Add brand, product recommendations, and related data retrieval

--- src/hook/products/view-products-detalis-hook.js
+++ src/hook/products/view-products-detalis-hook.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getOneProduct } from "../../redux/actions/productsAction";
+import {
+  getOneProduct,
+  getProductLike,
+} from "../../redux/actions/productsAction";
 import mobile from "../../images/mobile.png";
 import { getOneCategory } from "../../redux/actions/categoryAction";
+import { getOneBrand } from "../../redux/actions/brandAction";
 
 const ViewProductsDetalisHook = (prodID) => {
   const dispatch = useDispatch();
@@ -12,6 +16,8 @@ const ViewProductsDetalisHook = (prodID) => {
 
   const oneProducts = useSelector((state) => state.allproducts.oneProduct);
   const oneCategory = useSelector((state) => state.allCategory.oneCategory);
+  const oneBrand = useSelector((state) => state.allBrand.oneBrand);
+  const productLike = useSelector((state) => state.allproducts.productLike);
 
   //to show products item
   let item = [];
@@ -20,6 +26,8 @@ const ViewProductsDetalisHook = (prodID) => {
 
   useEffect(() => {
     if (item.category) dispatch(getOneCategory(item.category));
+    if (item.brand) dispatch(getOneBrand(item.brand));
+    if (item.category) dispatch(getProductLike(item.category));
   }, [item]);
 
   //to view images gallery
@@ -43,7 +51,16 @@ const ViewProductsDetalisHook = (prodID) => {
   if (oneCategory.data) cat = oneCategory.data;
   else cat = [];
 
-  return [item, cat, images];
+  //to show brand item
+  let brand = [];
+  if (oneBrand.data) brand = oneBrand.data;
+  else brand = [];
+
+  let prod = [];
+  if (productLike) prod = productLike.data;
+  else prod = [];
+
+  return [item, cat, images, brand, prod];
 };
 
 export default ViewProductsDetalisHook;
