488fa7304762dbc608824c11cb7f43c6b78b2438
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-10-31 15:19:14 +0300

    Integrate `ViewProductsDetalisHook` into `ProductDetalisPage` to fetch product, category, brand, and recommendations

--- src/page/Products/ProductDetalisPage.js
+++ src/page/Products/ProductDetalisPage.js
@@ -4,15 +4,17 @@ import { Container } from "react-bootstrap";
 import ProductDetails from "../../Component/Products/ProductDetails";
 import RateContainer from "../../Component/Rate/RateContainer";
 import CardProductContainer from "./../../Component/Products/CardProductContainer";
+import ViewProductsDetalisHook from "../../hook/products/view-products-detalis-hook";
 import { useParams } from "react-router-dom";
 
 const ProductDetalisPage = () => {
   const { id } = useParams();
+  const [item, cat, images, brand, prod] = ViewProductsDetalisHook(id);
   return (
     <div style={{ minHeight: "670px" }}>
       <CategoryHeader />
       <Container>
-        <ProductDetails id={id} />
+        <ProductDetails />
         <RateContainer />
         <CardProductContainer title="منتجات قد تعجبك" />
       </Container>
