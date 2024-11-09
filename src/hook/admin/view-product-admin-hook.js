35977b8001e4c9c23fc3068b70d84679c4bde7a0
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-11-09 14:08:31 +0200

    Refactor ViewProductAdminHook for improved structure and pagination handling

--- src/hook/admin/view-product-admin-hook.js
+++ src/hook/admin/view-product-admin-hook.js
@@ -1,31 +1,29 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts, getAllProductsPage } from "../../redux/actions/productsAction";
+import { getAllProducts } from "../../redux/actions/productsAction";
+import { getAllProductsPage } from "./../../redux/actions/productsAction";
 
-const viewProductAdminHook = () => {
+const ViewProductAdminHook = () => {
   const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(getAllProducts());
+    dispatch(getAllProducts(8));
   }, []);
 
   const onPress = async (page) => {
-    await dispatch(getAllProductsPage(page, 10));
+    await dispatch(getAllProductsPage(page, 8));
   };
-
-  const allProducts = useSelector((state) => state.allproducts.allProducts);
-
   let items = [];
-  if (allProducts.data) items = allProducts.data;
-  else items = [];
-
   let pagination = [];
-  if (allProducts.paginationResult)
-    pagination = allProducts.paginationResult;        ;
-  else 
-  pagination = [];
+  const allProducts = useSelector((state) => state.allproducts.allProducts);
+  try {
+    if (allProducts.data) items = allProducts.data;
+    else items = [];
 
+    if (allProducts.paginationResult)
+      pagination = allProducts.paginationResult.numberOfPages;
+    else pagination = [];
+  } catch (e) {}
   return [items, pagination, onPress];
 };
 
-export default viewProductAdminHook;
+export default ViewProductAdminHook;
