9bed597a58065dc6b9ff6b331c18429421678839
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-11-13 20:24:53 +0200

    Fix ViewHomeProductsHook: handle undefined state and dispatch dependencies

--- src/hook/products/view-home-products-hook.js
+++ src/hook/products/view-home-products-hook.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../../redux/actions/productsAction";
 
 const ViewHomeProductsHook = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getAllProducts());
-  }, []);
+  }, [dispatch]);
 
-  const allProducts = useSelector((state) => state.allproducts.allProducts);
+  const allProducts = useSelector((state) => state.allProducts?.allProducts);
 
   let items = [];
-  if (allProducts.data) items = allProducts.data.slice(0, 4);
-  else items = [];
+  if (allProducts && allProducts.data) {
+    items = allProducts.data.slice(0, 4);
+  } else {
+    items = [];
+  }
 
   return [items];
 };
