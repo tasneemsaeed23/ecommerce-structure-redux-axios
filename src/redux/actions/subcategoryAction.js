a37019555ac6e05af44b799e55aa1551af425422
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-11-14 23:02:49 +0200

    Refactored action creators for subcategory creation and retrieval, Standardized error handling and response payloads

--- src/redux/actions/subcategoryAction.js
+++ src/redux/actions/subcategoryAction.js
@@ -1,10 +1,11 @@
-import { CREATE_SUB_CATEGORY, GET_ERROR, GET_SUB_CATEGORY } from "../type";
-import { useInsertData } from "../../hooks/useInsertData";
+import { CREATE_SUB_CATEGORY, GET_SUB_CATEGORY, GET_ERROR } from "../type";
 import useGetData from "../../hooks/useGetData";
-//create Subcategory with pagination
-export const creatSubCategory = (data) => async (dispatch) => {
+import { useInsertData } from "../../hooks/useInsertData";
+
+//gcreate sub category with pagination
+export const createSubCategory = (data) => async (dispatch) => {
   try {
-    const response = await useInsertData(`/api/v1/subcategories`, data);
+    const response = await useInsertData("/api/v1/subcategories", data);
     dispatch({
       type: CREATE_SUB_CATEGORY,
       payload: response,
@@ -13,16 +14,16 @@ export const creatSubCategory = (data) => async (dispatch) => {
   } catch (e) {
     dispatch({
       type: GET_ERROR,
-      payload: `Error: ${e.message}`,
+      payload: "Error " + e,
     });
   }
 };
 
-//get sub category depend in vat id
+//get sub category depend in cat id
 export const getOneCategory = (id) => async (dispatch) => {
   try {
     const response = await useGetData(`/api/v1/categories/${id}/subcategories`);
-    console.log(response.data);
+
     dispatch({
       type: GET_SUB_CATEGORY,
       payload: response,
@@ -31,7 +32,7 @@ export const getOneCategory = (id) => async (dispatch) => {
   } catch (e) {
     dispatch({
       type: GET_ERROR,
-      payload: `Error: ${e.message}`,
+      payload: "Error " + e,
     });
   }
 };
