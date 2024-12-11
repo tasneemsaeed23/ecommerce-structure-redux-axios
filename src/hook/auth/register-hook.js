31e3d646e150fc1effde7f310ab0663cbe23f15f
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-11 20:10:35 +0200

    The updated RegisterHook adds Redux integration, better error handling, loading state management, and automatic redirection for a smoother user registration experience.

--- src/hook/auth/register-hook.js
+++ src/hook/auth/register-hook.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import notify from "./../useNotifaction";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewUser } from "../../redux/actions/authAction";
+import { useNavigate } from "react-router-dom";
 
 const RegisterHook = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -44,9 +49,55 @@ const RegisterHook = () => {
     }
   };
 
-  const onSubmit = (e) => {
+  const res = useSelector((state) => state.authReducer.createUser);
+  //save data
+  const onSubmit = async (e) => {
     validationValues();
+    setLoading(true);
+    await dispatch(
+      createNewUser({
+        name,
+        email,
+        phone,
+        password,
+        confirmPassword: confirmPassword,
+      })
+    );
+    setLoading(false);
   };
+
+  useEffect(() => {
+    if (loading === false) {
+      if (res) {
+        console.log(res);
+        if (res.data.token) {
+          localStorage.setItem("token", res.data.token);
+          notify("تم تسجيل الحساب بنجاح", "success");
+          setName("");
+          setTimeout(() => {
+            navigate("/login");
+          }, 2000);
+        }
+        if (res.data.errors) {
+          if (res.data.errors[0].msg === "User already exists") {
+            notify("هذا الايميل مسجل من قبل ", "error");
+          }
+        }
+
+        if (res.data.errors) {
+          if (res.data.errors[0].msg === "accept only egypt phone numbers") {
+            notify("يجب ان يكون الرقم مصري مكون من 11 رقم", "error");
+          }
+
+          if (res.data.errors) {
+            if (res.data.errors[0].msg === "must be at least 6 chars") {
+              notify("لأقل كلمة سر 6 احرف او ارقام", "error");
+            }
+          }
+        }
+      }
+    }
+  }, [loading]);
 
   return [
     name,
