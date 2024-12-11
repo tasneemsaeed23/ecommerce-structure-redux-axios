4ab4cb2f6382f1bb37f18d27eacf14a67bc81f1e
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-11 20:14:13 +0200

    Add authReducer to root reducer in combineReducers.

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import categoryReducer from "./categoryReducer";
 import brandReducer from "./brandReducer";
 import subcategoryReducer from "./subcategoryReducer";
 import productsReducer from "./productsReducer";
+import authReducer from "./authReducer";
 export default combineReducers({
   allCategory: categoryReducer,
   allBrand: brandReducer,
   subCategory: subcategoryReducer,
   allproducts: productsReducer,
+  authReducer: authReducer,
 });
