be92b0f63fba955f595fbea5b39b68f3b85c4504
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-11 20:12:54 +0200

    Add authReducer to handle CREATE_NEW_USER action for managing user registration state.

---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,21 @@
+import { CREATE_NEW_USER } from "../type";
+
+const inital = {
+  createUser: [],
+  loading: true,
+};
+
+const authReducer = (state = inital, action) => {
+  switch (action.type) {
+    case CREATE_NEW_USER:
+      return {
+        ...state,
+        createUser: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
