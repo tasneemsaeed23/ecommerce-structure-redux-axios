dfb9351d3bb92582c7a5f2c3f1d23bf83c329a74
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-11 21:08:40 +0200

    Enhance LoginPage with LoginHook, loading spinner, and password reset link for improved functionality.

--- src/page/Auth/LoginPage.js
+++ src/page/Auth/LoginPage.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import LoginHook from "./../../hook/auth/login-hook";
+import { ToastContainer } from "react-toastify";
 
 const LoginPage = () => {
+  const [
+    email,
+    password,
+    loading,
+    onChangeEmail,
+    onChangePassword,
+    onSubmit,
+    isPress,
+  ] = LoginHook();
   return (
-    <Container style={{ minHeight: "680px" }}>
+    <Container style={{ minHeight: "690px" }}>
       <Row className="py-5 d-flex justify-content-center ">
         <Col sm="12" className="d-flex flex-column ">
           <label className="mx-auto title-login">تسجيل الدخول</label>
           <input
+            value={email}
+            onChange={onChangeEmail}
             placeholder="الايميل..."
-            type="text"
+            type="email"
             className="user-input my-3 text-center mx-auto"
           />
           <input
+            value={password}
+            onChange={onChangePassword}
             placeholder="كلمه السر..."
             type="password"
             className="user-input text-center mx-auto"
           />
-          <button className="btn-login mx-auto mt-4">تسجيل الدخول</button>
+          <button onClick={onSubmit} className="btn-login mx-auto mt-4">
+            تسجيل الدخول
+          </button>
           <label className="mx-auto my-4">
             ليس لديك حساب ؟{" "}
             <Link to="/register" style={{ textDecoration: "none" }}>
@@ -27,22 +44,24 @@ const LoginPage = () => {
               </span>
             </Link>
           </label>
+
+          <label className="mx-auto my-4">
+            <Link
+              to="/user/forget-password"
+              style={{ textDecoration: "none", color: "red" }}
+            >
+              هل نسيت كلمه السر
+            </Link>
+          </label>
+
+          {isPress === true ? (
+            loading === true ? (
+              <Spinner animation="border" role="status"></Spinner>
+            ) : null
+          ) : null}
         </Col>
-
-        <label className="mx-auto my-4">
-          <Link to="/admin/allproducts" style={{ textDecoration: "none" }}>
-            <span style={{ cursor: "pointer" }} className="text-danger">
-              الدخول ادمن
-            </span>
-          </Link>
-
-          <Link to="/user/allorders" style={{ textDecoration: "none" }}>
-            <span style={{ cursor: "pointer" }} className="text-danger mx-3">
-              الدخول مستخدم
-            </span>
-          </Link>
-        </label>
       </Row>
+      <ToastContainer />
     </Container>
   );
 };
