de2cd1b3a75b697790a781a18a01f89ef248fc70
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-14 18:21:37 +0200

    "Add useGetDataToken for authenticated API requests."

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -5,4 +5,13 @@ const useGetData = async (url, parmas) => {
   return res.data;
 };
 
-export default useGetData;
+const useGetDataToken = async (url, parmas) => {
+  const config = {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  };
+
+  const res = await baseURL.get(url, config);
+  return res.data;
+};
+
+export { useGetData, useGetDataToken };
