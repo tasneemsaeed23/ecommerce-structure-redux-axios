f276bfd2893a192b8a14484f46ee96f4bc0d19ac
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-14 18:21:04 +0200

    Update NavBarLogin for dynamic user handling and role-based navigation

--- src/Component/Uitlity/NavBarLogin.js
+++ src/Component/Uitlity/NavBarLogin.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { Navbar, Container, FormControl, Nav } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import {
+  Navbar,
+  Container,
+  FormControl,
+  Nav,
+  NavDropdown,
+} from "react-bootstrap";
 import logo from "../../images/logo.png";
 import login from "../../images/login.png";
 import cart from "../../images/cart.png";
-import NavbarSearchHook from "../../hook/Search/navbar-search-hook";
+import NavbarSearchHook from "./../../hook/search/navbar-search-hook";
+import { useDispatch, useSelector } from "react-redux";
+import { getLoggedUser } from "./../../redux/actions/authAction";
 const NavBarLogin = () => {
+  //const dispatch = useDispatch()
+
   const [OnChangeSearch, searchWord] = NavbarSearchHook();
   let word = "";
-  if (localStorage.setItem("searchWord") != null) {
+  if (localStorage.getItem("searchWord") != null)
     word = localStorage.getItem("searchWord");
-  }
+
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    if (localStorage.getItem("user") != null)
+      setUser(JSON.parse(localStorage.getItem("user")));
+  }, []);
+
+  const logOut = () => {
+    localStorage.removeItem("user");
+    setUser("");
+  };
+
   return (
     <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
       <Container>
@@ -29,13 +50,32 @@ const NavBarLogin = () => {
             aria-label="Search"
           />
           <Nav className="me-auto">
-            <Nav.Link
-              href="/login"
-              className="nav-text d-flex mt-3 justify-content-center"
-            >
-              <img src={login} className="login-img" alt="sfvs" />
-              <p style={{ color: "white" }}>دخول</p>
-            </Nav.Link>
+            {user !== "" ? (
+              <NavDropdown title={user.name} id="basic-nav-dropdown">
+                {user.role === "admin" ? (
+                  <NavDropdown.Item href="/admin/allproducts">
+                    لوحة التحكم
+                  </NavDropdown.Item>
+                ) : (
+                  <NavDropdown.Item href="/user/profile">
+                    الصفحه الشخصية
+                  </NavDropdown.Item>
+                )}
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={logOut} href="/">
+                  تسجيل خروج
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <Nav.Link
+                href="/login"
+                className="nav-text d-flex mt-3 justify-content-center"
+              >
+                <img src={login} className="login-img" alt="sfvs" />
+                <p style={{ color: "white" }}>دخول</p>
+              </Nav.Link>
+            )}
+
             <Nav.Link
               href="/cart"
               className="nav-text d-flex mt-3 justify-content-center"
