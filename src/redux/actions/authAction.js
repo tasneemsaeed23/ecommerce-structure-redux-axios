9d9ef569daaf97152b991357ad4dd06fb948fc36
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-14 18:22:48 +0200

    Add loginUser and getLoggedUser actions for authentication handling.

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,5 +1,6 @@
-import { CREATE_NEW_USER } from "../type";
+import { CREATE_NEW_USER, LOGIN_USER, GET_CURERNT_USER } from "../type";
 import { useInsertData } from "../../hooks/useInsertData";
+import { useGetDataToken } from "../../hooks/useGetData";
 
 export const createNewUser = (data) => async (dispatch) => {
   try {
@@ -12,6 +13,40 @@ export const createNewUser = (data) => async (dispatch) => {
   } catch (e) {
     dispatch({
       type: CREATE_NEW_USER,
+      payload: e.response,
+    });
+  }
+};
+
+//login  user
+export const loginUser = (data) => async (dispatch) => {
+  try {
+    const response = await useInsertData(`/api/v1/auth/login`, data);
+    dispatch({
+      type: LOGIN_USER,
+      payload: response,
+      loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: LOGIN_USER,
+      payload: e.response,
+    });
+  }
+};
+
+//login  user
+export const getLoggedUser = () => async (dispatch) => {
+  try {
+    const response = await useGetDataToken(`/api/v1/users/getMe`);
+    dispatch({
+      type: GET_CURERNT_USER,
+      payload: response,
+      loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_CURERNT_USER,
       payload: e.response,
     });
   }
