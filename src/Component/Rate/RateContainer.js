d379f13b6d58904b5cc1c0b19b1928be4a2dd85b
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-16 18:42:28 +0200

    Integrate dynamic reviews and pagination in RateContainer component.

--- src/Component/Rate/RateContainer.js
+++ src/Component/Rate/RateContainer.js
@@ -4,25 +4,47 @@ import rate from "../../images/rate.png";
 import RateItem from "./RateItem";
 import RatePost from "./RatePost";
 import Pagination from "../Uitlity/Pagination";
+import { useParams } from "react-router-dom";
+import ViewAllReviewHook from "../../hook/review/view-all-review-hook";
 
-const RateContainer = () => {
+const RateContainer = ({ rateAvg, rateQty }) => {
+  const { id } = useParams();
+  const [allReview, onPress] = ViewAllReviewHook(id);
+
   return (
     <Container className="rate-container">
       <Row>
         <Col className="d-flex">
           <div className="sub-tile d-inline p-1 ">التقيمات</div>
           <img className="mt-2" src={rate} alt="" height="16px" width="16px" />
-          <div className="cat-rate  d-inline  p-1 pt-2">4.3</div>
-          <div className="rate-count d-inline p-1 pt-2">(160 تقييم)</div>
+          <div className="cat-rate  d-inline  p-1 pt-2">{rateAvg}</div>
+          <div className="rate-count d-inline p-1 pt-2">
+            ({`${rateQty}  تقيم`})
+          </div>
         </Col>
       </Row>
-      <RatePost />
-      <RateItem />
-      <RateItem />
-      <RateItem />
-      <RateItem />
 
-      <Pagination />
+      <RatePost />
+
+      {allReview.data ? (
+        allReview.data.map((review, index) => {
+          return <RateItem key={index} review={review} />;
+        })
+      ) : (
+        <h6>لا يوجد تقيمات الان</h6>
+      )}
+
+      {allReview.paginationResult &&
+      allReview.paginationResult.numberOfPages >= 2 ? (
+        <Pagination
+          pageCount={
+            allReview.paginationResult
+              ? allReview.paginationResult.numberOfPages
+              : 0
+          }
+          onPress={onPress}
+        />
+      ) : null}
     </Container>
   );
 };
