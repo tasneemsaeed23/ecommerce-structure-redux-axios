ecc4fad4bf79f633092ab61ae2ce87987e58754d
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-17 19:15:28 +0200

    `feat: updated UserFavoriteProduct component to fetch wishlist data from Redux and display products in a CardProductsContainer`

--- src/Component/Products/ProductCard.js
+++ src/Component/Products/ProductCard.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Col } from "react-bootstrap";
-import favoff from "../../images/fav-off.png";
+
 import rate from "../../images/rate.png";
 import { Link } from "react-router-dom";
-const ProductCard = ({ item }) => {
+
+import { ToastContainer } from "react-toastify";
+import ProductCardHook from "./../../hook/products/product-card-hook";
+
+const ProductCard = ({ item, favProd }) => {
+  const [removeToWishListData, addToWishListData, handelFav, favImg] =
+    ProductCardHook(item, favProd);
+
   return (
     <Col xs="6" sm="6" md="4" lg="3" className="d-flex">
       <Card
@@ -25,12 +32,14 @@ const ProductCard = ({ item }) => {
         </Link>
         <div className="d-flex justify-content-end mx-2">
           <img
-            src={favoff}
+            src={favImg}
             alt=""
+            onClick={handelFav}
             className="text-center"
             style={{
               height: "24px",
               width: "26px",
+              cursor: "pointer",
             }}
           />
         </div>
@@ -48,7 +57,7 @@ const ProductCard = ({ item }) => {
                   height="16px"
                   width="16px"
                 />
-                <div className="card-rate mx-2">{item.ratingsQuantity}</div>
+                <div className="card-rate mx-2">{item.ratingsAverage || 0}</div>
               </div>
               <div className="d-flex">
                 <div className="card-price">{item.price}</div>
@@ -58,6 +67,7 @@ const ProductCard = ({ item }) => {
           </Card.Text>
         </Card.Body>
       </Card>
+      <ToastContainer />
     </Col>
   );
 };
