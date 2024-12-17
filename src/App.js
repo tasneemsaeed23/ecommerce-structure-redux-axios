55c06866b149eae154e75f488c8f942f1786f6e4
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-17 19:14:52 +0200

    `feat: Added new features to admin sidebar, product card, and user favorite product page, including adding coupons and user addresses, and implemented Redux actions and reducers for these features.`

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import Footer from "./Component/Uitlity/Footer";
-import NavBarLogin from "./Component/Uitlity/NavBarLogin";
-import HomePage from "./page/Home/HomePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "./page/Auth/LoginPage";
-import RegisterPage from "./page/Auth/RegisterPage";
-import AllCategoryPage from "./page/Category/AllCategoryPage";
-import AllBrandPage from "./page/Brand/AllBrandPage";
-import ShopProductsPage from "./page/Products/ShopProductsPage";
-import ProductDetalisPage from "./page/Products/ProductDetalisPage";
-import CartPage from "./page/Cart/CartPage";
-import ChoosePayMethoudPage from "./page/Checkout/ChoosePayMethoudPage";
-import AdminAllProductPage from "./page/Admin/AdminAllProductPage";
-import AdminAllOrdersPage from "./page/Admin/AdminAllOrdersPage";
-import AdminOrderDetalisPage from "./page/Admin/AdminOrderDetalisPage";
-import AdminAddBrandPage from "./page/Admin/AdminAddBrandPage";
-import AdminAddCategoryPage from "./page/Admin/AdminAddCategoryPage";
-import AdminAddSubCategoryPage from "./page/Admin/AddSubCategoryPage";
-import AdminAddProductsPage from "./page/Admin/AdminAddProductsPage";
-import UserAllOrdersPage from "./page/User/UserAllOrderPage";
-import UserFavoriteProductsPage from "./page/User/UserFavoriteProductsPage";
-import UserAllAddresPage from "./page/User/UserAllAddresPage";
-import UserAddAddressPage from "./page/User/UserAddAddressPage";
-import UserEditAddressPage from "./page/User/UserEditAddressPage";
-import UserProfilePage from "./page/User/UserProfilePage";
-import AdminEditProductPage from "./page/Admin/AdminEditProductPage";
-import ForgetPasswordPage from "./page/Auth/forgetPasswordPage";
-import VerifyPasswordPage from "./page/Auth/VerifyPasswordPage";
-import RsetPasswordPage from "./page/Auth/ResetPasswordPage";
-
+import HomePage from "./Page/Home/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBarLogin from "./Components/Uitily/NavBarLogin";
+import Footer from "./Components/Uitily/Footer";
+import LoginPage from "./Page/Auth/LoginPage";
+import RegisterPage from "./Page/Auth/RegisterPage";
+import AllCategoryPage from "./Page/Category/AllCategoryPage";
+import AllBrandPage from "./Page/Brand/AllBrandPage";
+import ShopProductsPage from "./Page/Products/ShopProductsPage";
+import ProductDetalisPage from "./Page/Products/ProductDetalisPage";
+import CartPage from "./Page/Cart/CartPage";
+import ChoosePayMethoudPage from "./Page/Checkout/ChoosePayMethoudPage";
+import AdminAllProductsPage from "./Page/Admin/AdminAllProductsPage";
+import AdminAllOrdersPage from "./Page/Admin/AdminAllOrdersPage";
+import AdminOrderDetalisPage from "./Page/Admin/AdminOrderDetalisPage";
+import AdminAddBrandPage from "./Page/Admin/AdminAddBrandPage";
+import AdminAddCategoryPage from "./Page/Admin/AdminAddCategoryPage";
+import AdminAddSubCategoryPage from "./Page/Admin/AdminAddSubCategoryPage";
+import AdminAddProductsPage from "./Page/Admin/AdminAddProductsPage";
+import UserAllOrdersPage from "./Page/User/UserAllOrdersPage";
+import UserFavoriteProductsPage from "./Page/User/UserFavoriteProductsPage";
+import UserAllAddresPage from "./Page/User/UserAllAddresPage";
+import UserAddAddressPage from "./Page/User/UserAddAddressPage";
+import UserEditAddressPage from "./Page/User/UserEditAddressPage";
+import UserProfilePage from "./Page/User/UserProfilePage";
+import AdminEditProductsPage from "./Page/Admin/AdminEditProductsPage";
+import ForgetPasswordPage from "./Page/Auth/ForgetPasswordPage";
+import VerifyPasswordPage from "./Page/Auth/VerifyPasswordPage";
+import RsetPasswordPage from "./Page/Auth/ResetPasswordPage";
+import AdminEditCouponPage from "./Page/Admin/AdminEditCouponPage";
+import AdminAddCouponPage from "./page/Admin/AdminAddCouponPage";
 function App() {
   return (
     <div className="font">
       <NavBarLogin />
-
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Login" element={<LoginPage />} />
-          <Route path="/Register" element={<RegisterPage />} />
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/allcategory" element={<AllCategoryPage />} />
           <Route path="/allbrand" element={<AllBrandPage />} />
           <Route path="/products" element={<ShopProductsPage />} />
           <Route path="/products/:id" element={<ProductDetalisPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/order/paymethoud" element={<ChoosePayMethoudPage />} />
-          <Route path="/admin/allproducts" element={<AdminAllProductPage />} />
+          <Route path="/admin/allproducts" element={<AdminAllProductsPage />} />
           <Route path="/admin/allorders" element={<AdminAllOrdersPage />} />
           <Route path="/admin/orders/:id" element={<AdminOrderDetalisPage />} />
           <Route path="/admin/addbrand" element={<AdminAddBrandPage />} />
@@ -54,27 +54,32 @@ function App() {
             element={<AdminAddSubCategoryPage />}
           />
           <Route path="/admin/addproduct" element={<AdminAddProductsPage />} />
+          <Route path="/admin/addcoupon" element={<AdminAddCouponPage />} />
+          <Route
+            path="/admin/editcoupon/:id"
+            element={<AdminEditCouponPage />}
+          />
           <Route path="/user/allorders" element={<UserAllOrdersPage />} />
           <Route
             path="/user/favoriteproducts"
             element={<UserFavoriteProductsPage />}
           />
           <Route path="/user/addresses" element={<UserAllAddresPage />} />
-
           <Route path="/user/add-address" element={<UserAddAddressPage />} />
-          <Route path="/user/edit-address" element={<UserEditAddressPage />} />
+          <Route
+            path="/user/edit-address/:id"
+            element={<UserEditAddressPage />}
+          />
           <Route path="/user/profile" element={<UserProfilePage />} />
           <Route
             path="/admin/editproduct/:id"
-            element={<AdminEditProductPage />}
+            element={<AdminEditProductsPage />}
           />
           <Route
             path="/user/forget-password"
             element={<ForgetPasswordPage />}
           />
-
           <Route path="/user/verify-code" element={<VerifyPasswordPage />} />
-
           <Route path="/user/reset-password" element={<RsetPasswordPage />} />
         </Routes>
       </BrowserRouter>
