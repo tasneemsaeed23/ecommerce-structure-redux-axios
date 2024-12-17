cb9ad889bb5959ccc2d175d47f885b11207c058f
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-12-17 19:15:22 +0200

    `feat: Added functionality to display user's favorite products, implemented product card hook, and updated reducers and added new reducers and actions for user addresses and coupons`

--- src/Component/Products/CardProductContainer.js
+++ src/Component/Products/CardProductContainer.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import SubTiltle from "../Uitlity/SubTiltle";
+import SubTiltle from "../Uitily/SubTiltle";
 import ProductCard from "./ProductCard";
+import CardContainerHook from "./../../hook/products/card-container-hook";
 
 const CardProductsContainer = ({ title, btntitle, pathText, products }) => {
+  const [favProd] = CardContainerHook();
   return (
     <Container>
       {products ? (
         <SubTiltle title={title} btntitle={btntitle} pathText={pathText} />
       ) : null}
       <Row className="my-2 d-flex justify-content-between">
-        {console.log("Products:", products)}
         {products
           ? products.map((item, index) => (
-              <ProductCard key={index} item={item} />
+              <ProductCard favProd={favProd} key={index} item={item} />
             ))
           : null}
       </Row>
